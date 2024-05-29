e403b6028e4e0f6cc01603e9f6079265a0b19c38
Shiloh <s1042952@gm.pu.edu.tw>
2024-05-30 01:18:54 +0800

    0530 網頁版完成

--- all.js
+++ all.js
@@ -8,3 +8,24 @@ $(document).ready(function() {
         $('body').toggleClass('shieldshow')
     })
 })
+
+$(document).ready(function() {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 24,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+});
